cf92ca0002e5b7e320d4152575c6eec0a410a531
TakuyaKinoshita <kinoshita@lm-labs.com>
2024-01-15 15:59:28 +0900

    まっさらな開発環境構築

--- docker/php/wp-install.sh
+++ docker/php/wp-install.sh
@@ -6,10 +6,16 @@ if [ -f wp-config.php ]; then
     echo "WordPressはすでにインストールされています。php-fpmを起動します。"
     php-fpm
 else
+    # Add the following lines in the wp-install.sh script before the WordPress installation part
+    # Clean up existing WordPress files
+    rm -rf ./*
+    # Make the directory writable
+    chmod -R 777 .
     wp --info
     wp core download --allow-root
-    wp config create --dbname="${MYSQL_DATABASE_NAME}" --dbuser="${MYSQL_DATABASE_USER}" --dbpass="${MYSQL_DATABESE_PASSWORD}" --allow-root
-    wp db create --allow-root
+    wp config create --dbhost="${MYSQL_HOST_NAME}:${MYSQL_PORT}" --dbname="${MYSQL_DATABASE_NAME}" --dbuser="${MYSQL_DATABASE_USER}" --dbpass="${MYSQL_DATABESE_PASSWORD}" --allow-root
+    # wp config create --dbname="${MYSQL_DATABASE_NAME}" --dbuser="${MYSQL_DATABASE_USER}" --dbpass="${MYSQL_DATABESE_PASSWORD}" --allow-root
+    wp db create
     wp core install \
     --url="http://${WORDPRESS_LOCAL_URL}:${WORDPRESS_LOCAL_PORT}" \
     --title="${WORDPRESS_SITE_TITLE}" \
